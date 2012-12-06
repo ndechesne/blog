847f21c7baaac8f7a731a00f27bf1bcfdfdad00a
Nicolas Dechesne <n-dechesne@ti.com>
2012-12-06 16:27:48 +0100

    initial commit
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cf72ebdd0e14a01126042610208ccd4edd4516f2220c00e29d000bb3baea1880965b40943abff1c8669edf392a13a35e617dd76ba23ad44470edf0dcbce85cfb'
